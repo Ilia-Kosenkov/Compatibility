9c07b409e87144b8d19690d0a2a4b9df5d4741dd
Ilia <ilia.kosenkov.at.gm@gmail.com>
2020-01-10 15:30:58 +0200

    Primitive benchmarking

---
+++ tests/Benchmarks/RecycledStringBenchmarks.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
+using TextExtensions;
+
+namespace Benchmarks
+{
+    [SimpleJob(RuntimeMoniker.NetCoreApp31)]
+    [MemoryDiagnoser]
+    public class RecycledStringBenchmarks
+    {
+        private char[] _data;
+
+        [Params(1000)]
+        public int N;
+
+        [Params(1000, 10_000)]
+        public int M;
+
+        [GlobalSetup]
+        public void GlobalSetup()
+        {
+            _data = new char[10 * N];
+
+            for (var i = 0; i < N; i++)
+                _data.AsSpan(10 * i, 10).Fill((char)('A' + i % 50));
+        }
+
+        [Benchmark(Baseline = true)]
+        public void MangedString()
+        {
+            string s = null;
+
+            for (var i = 0; i < M; i++)
+            {
+                var id = i % N;
+                s = _data.AsSpan(10 * id, 10).ToString();
+            }
+        }
+
+        [Benchmark]
+        public void RecycledString()
+        {
+
+            var rs = new RecycledString(10);
+
+            for (var i = 0; i < M; i++)
+            {
+                var id = i % N;
+                rs.TryCopy(_data.AsSpan(10 * id, 10));
+            }
+        }
+    }
+}
