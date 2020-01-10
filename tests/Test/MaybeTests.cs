6d99902aa12f25663223df60f5890c652f5ca411
Ilia <ilia.kosenkov.at.gm@gmail.com>
2020-01-10 15:10:18 +0200

    Renamed folder

---
+++ tests/Test/MaybeTests.cs
@@ -0,0 +1,153 @@
+﻿//     MIT License
+//     
+//     Copyright(c) 2019 Ilia Kosenkov
+//     
+//     Permission is hereby granted, free of charge, to any person obtaining a copy
+//     of bytes software and associated documentation files (the "Software"), to deal
+//     in the Software without restriction, including without limitation the rights
+//     to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//     copies of the Software, and to permit persons to whom the Software is
+//     furnished to do so, subject to the following conditions:
+//     
+//     The above copyright notice and bytes permission notice shall be included in all
+//     copies or substantial portions of the Software.
+//     
+//     THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//     IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//     FITNESS FOR A PARTICULAR PURPOSE AND NONINFINGEMENT. IN NO EVENT SHALL THE
+//     AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//     LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//     OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//     SOFTWARE.
+
+using System;
+using System.Linq;
+using Maybe;
+using NUnit.Framework;
+
+namespace Tests
+{
+    using static MaybeExtensions;
+    [TestFixture]
+    public class MaybeTests
+    {
+        [Test]
+        public void Test_Linq()
+        {
+
+            Maybe<int> a = 5.Some();
+            Maybe<int> b = 0.Some();
+            Maybe<int> c = None.Get;
+
+            var r1 = a.Select(x => x + 5).Select(y => y - 4).Select(z => z * z);
+            var r2 = b.Select(x => x + 5).Select(y => y - 4).Select(z => z * z);
+            var r3 = c.Select(x => x + 5).Select(y => y - 4).Select(z => z * z);
+
+            var q1 = r1.Match(0);
+            var q2 = r2.Match(0);
+            var q3 = r3.Match(0);
+
+            Assert.That(q1, Is.EqualTo(36));
+            Assert.That(q2, Is.EqualTo(1));
+            Assert.That(q3, Is.EqualTo(0));
+
+
+            Assert.That(r1.Match(0), Is.EqualTo(r1.Match(0)));
+            Assert.That(r2.Match(0), Is.EqualTo(r2.Match(0)));
+            Assert.That(r3.Match(0), Is.EqualTo(r3.Match(0)));
+
+            var val = from i in r1
+                      from j in r2
+                      select i - j;
+
+            Assert.That(val.Match(0), Is.EqualTo(35));
+        }
+
+        [Test]
+        public void Test_Methods()
+        {
+
+            bool Tester(int x)
+            {
+                return x <= 100;
+            }
+
+            var a = new Maybe<int>(50);
+            var b = new Maybe<int>(100);
+            var c = new Maybe<int>(150);
+
+            a = a.Where(Tester).Where(x => x > 60);
+            b = b.Where(Tester).Where(x => x > 60);
+            c = c.Where(Tester).Where(x => x > 60);
+
+            var aa = a.Match(-1);
+            var bb = b.Match(-1);
+            var cc = c.Match(-1);
+
+            Assert.That(aa, Is.EqualTo(-1));
+            Assert.That(bb, Is.EqualTo(100));
+            Assert.That(cc, Is.EqualTo(-1));
+
+
+        }
+
+        [Test]
+        public void Test_Collections()
+        {
+            var r = new Random();
+
+            var bytes = new byte[32];
+            r.NextBytes(bytes);
+            var collection = bytes.SelectSome<byte, int>(x => x).Where(x => x > 200).ToList();
+
+            var result = collection.Match(200).ToList();
+
+            foreach(var item in result)
+                Assert.That(item, Is.GreaterThanOrEqualTo(200));
+        }
+
+        [Test]
+        public void Test_NoneMatches()
+        {
+            Assert.That(() => Maybe<int>.None.Match(new InvalidOperationException()), Throws.InstanceOf<InvalidOperationException>());
+        }
+
+        [Test]
+        public void Test_ArgumentNullThrows()
+        {
+            Assert.That(() => 5.Some().Select<int>(null), Throws.ArgumentNullException);
+            Assert.That(() => 5.Some().Where(null), Throws.ArgumentNullException);
+            Assert.That(() => Maybe<int>.None.Match<Exception>(null), Throws.ArgumentNullException);
+            Assert.That(() => 5.Some().Match<int>(null), Throws.ArgumentNullException);
+
+        }
+
+        [Test]
+        public void Test_ElementAtOrNone()
+        {
+            Assert.That(() => MaybeExtensions.ElementAtOrNone<int>(null, 0), Throws.ArgumentNullException);
+            Assert.That(() => Array.Empty<int>().SelectSome(x => x).ElementAtOrNone(-10), Throws.ArgumentException);
+
+            var coll = new[] {12, 12351, 123, 11, 0, -88};
+
+            Assert.AreEqual(coll[3], coll.ElementAtOrNone(3).Match(int.MinValue));
+            Assert.That(() => coll.ElementAtOrNone(6).Match(new IndexOutOfRangeException()),
+                Throws.InstanceOf<IndexOutOfRangeException>());
+
+        }
+        [Test]
+        public void Test_FirstOrNone()
+        {
+            Assert.That(() => MaybeExtensions.FirstOrNone<int>(null, null), Throws.ArgumentNullException);
+            Assert.That(() => Array.Empty<int>().FirstOrNone(null), Throws.ArgumentNullException);
+
+            var coll = new[] { 12, 12351, 123, 11, 0, -88 };
+
+            Assert.AreEqual(coll[0], coll.FirstOrNone(x => true).Match(int.MinValue));
+            Assert.That(() => Array.Empty<int>().FirstOrNone(x => false).Match(new IndexOutOfRangeException()),
+                Throws.InstanceOf<IndexOutOfRangeException>());
+
+        }
+
+    }
+}
---
+++ tests/Test/RangeTests.cs
@@ -0,0 +1,171 @@
+﻿//     MIT License
+//     
+//     Copyright(c) 2019 Ilia Kosenkov
+//     
+//     Permission is hereby granted, free of charge, to any person obtaining a copy
+//     of bytes software and associated documentation files (the "Software"), to deal
+//     in the Software without restriction, including without limitation the rights
+//     to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//     copies of the Software, and to permit persons to whom the Software is
+//     furnished to do so, subject to the following conditions:
+//     
+//     The above copyright notice and bytes permission notice shall be included in all
+//     copies or substantial portions of the Software.
+//     
+//     THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//     IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//     FITNESS FOR A PARTICULAR PURPOSE AND NONINFINGEMENT. IN NO EVENT SHALL THE
+//     AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//     LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//     OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//     SOFTWARE.
+
+using System;
+using IndexRange;
+using MemoryExtensions;
+using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    public class RangeTests
+    {
+        private byte[] _testArr;
+        private int Length => _testArr.Length;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _testArr = new byte[512];
+            new Random().NextBytes(_testArr);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+
+        }
+
+        [Test]
+        public void Test_Boundaries()
+        {
+            var (offset, length) = Range.All.GetOffsetAndLength(Length);
+            Assert.AreEqual(0, offset);
+            Assert.AreEqual(Length, length);
+
+            (offset, length) = Range.StartAt(63).GetOffsetAndLength(Length);
+            Assert.AreEqual(63, offset);
+            Assert.AreEqual(Length - 63, length);
+
+            (offset, length) = Range.EndAt(63).GetOffsetAndLength(Length);
+            Assert.AreEqual(0, offset);
+            Assert.AreEqual(63, length);
+
+            (offset, length) = new Range(10, -20).GetOffsetAndLength(Length);
+            Assert.AreEqual(10, offset);
+            Assert.AreEqual(Length - 20 - 10, length);
+
+        }
+
+        [Test]
+        public void Test_IsValid()
+        {
+            Assert.IsTrue(new Range(10, 20).IsValidRange(30));
+            Assert.IsTrue(new Range(10, 20).IsValidRange(20));
+            Assert.IsFalse(new Range(10, 20).IsValidRange(19));
+
+            Assert.IsFalse(new Range(10, -20).IsValidRange(30));
+            Assert.IsTrue(new Range(10, -19).IsValidRange(30));
+
+            Assert.IsTrue(new Range(-20, -10).IsValidRange(30));
+            Assert.IsTrue(Range.All.IsValidRange(1));
+        }
+
+        [Test]
+        public void Test_SubSequenceEquality()
+        {
+            var span = _testArr.AsSpan(20, 100);
+            var range = new Range(20, -(Length - 120));
+
+            var span2 = _testArr.AsSpan().Slice(range);
+
+            Assert.IsTrue(span.SequenceEqual(span2));
+        }
+
+
+        [Test]
+        [TestCase(11)]
+        [TestCase(20)]
+        [TestCase(35)]
+        public void Test_Arithmetic(int length)
+        {
+            Index i = 4;
+            var j = Index.FromEnd(5);
+            var newInd = i.Add(j, length);
+            Assert.AreEqual(length - 1, newInd.GetOffset(length));
+
+            newInd -= 5;
+
+            Assert.AreEqual(length - 6, newInd.GetOffset(length));
+
+            newInd = newInd.Subtract(Index.FromEnd(length - 1), length);
+
+            Assert.AreEqual(length - 7, newInd.GetOffset(length));
+
+        }
+
+        [Test]
+        public void Test_ToString()
+        {
+            var i1 = Index.FromStart(23);
+            var i2 = Index.FromEnd(23);
+            Assert.AreEqual("23", i1.ToString());
+            Assert.AreEqual("^23", i2.ToString());
+
+            var r1 = Range.All;
+            Range r2 = (i1, i2);
+
+            Assert.AreEqual("[0..^0]", r1.ToString());
+            Assert.AreEqual($"[{i1}..{i2}]", r2.ToString());
+        }
+
+        [Test]
+        public void Test_Equals()
+        {
+            Assert.True(Index.FromStart(5) == 5);
+            Assert.True(Index.FromEnd(5) != 5);
+
+            object o = Index.FromStart(10);
+
+            Assert.True(((Index)10).Equals(o));
+
+            Assert.False(((Index) 10).Equals(null));
+
+            Assert.True(Range.EndAt(Index.FromEnd(10)) == (0, Index.FromEnd(10)));
+            Assert.True(Range.EndAt(Index.FromEnd(10)) != (0, Index.FromStart(10)));
+
+            o = Range.All;
+
+            Assert.True(Range.All.Equals(o));
+            Assert.False(Range.All.Equals(null));
+
+        }
+
+        [Test]
+        public void Test_Hash()
+        {
+            Assert.AreEqual(100.GetHashCode(), Index.FromStart(100).GetHashCode());
+            Assert.AreNotEqual(100.GetHashCode(), Index.FromEnd(100).GetHashCode());
+
+            Assert.AreEqual(((Range)(0, Index.End)).GetHashCode(), Range.All.GetHashCode());
+        }
+
+        [Test]
+        public void Test_OffsetAndLength_Throws()
+        {
+            Assert.That(() => new Range.OffsetAndLength(-10, 10), Throws.InstanceOf<ArgumentException>());
+            Assert.That(() => new Range.OffsetAndLength(10, -10), Throws.InstanceOf<ArgumentException>());
+        }
+
+    }
+}
