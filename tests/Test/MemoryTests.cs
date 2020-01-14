0833d7592df2f8c2553ebf0f1f4969bca1137547
Ilia <ilia.kosenkov.at.gm@gmail.com>
2020-01-14 13:00:53 +0200

    Accessors tests

--- tests/Test/MemoryTests.cs
+++ tests/Test/MemoryTests.cs
@@ -35,7 +35,7 @@ namespace Tests
         [SetUp]
         public void SetUp()
         {
-            _input = " \t    1111    klfhakljfh01928091-__IU1IJHJK  1JHR KJ       \t 1111   ";
+            _input = " \t   9191    klfhakljfh01928091-__IU1IJHJK  1JHR KJ       \t 1111   ";
             _inputArr = _input.ToCharArray();
         }
 
@@ -50,5 +50,29 @@ namespace Tests
             Assert.AreEqual(s1, s3);
         }
 
+        [Test]
+        public void TestAccessors()
+        {
+            var span = _inputArr.AsSpan();
+            var roSpan = _inputArr.AsReadOnlySpan();
+
+            Assume.That(span[8] == '1');
+
+            Assert.That(span.Get(8) == '1');
+            Assert.That(roSpan.Get(8) == '1');
+
+            Assert.That(span.At(8) == '1');
+            Assert.That(roSpan.At(8) == '1');
+
+            span.At(8) = '0';
+
+            Assume.That(span[8] == '0');
+
+            Assert.That(span.Get(8) == '0');
+            Assert.That(roSpan.Get(8) == '0');
+
+            Assert.That(span.At(8) == '0');
+            Assert.That(roSpan.At(8) == '0');
+        }
     }
 }
